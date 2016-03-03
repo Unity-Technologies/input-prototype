49b03f3f5717913196f2276439cb2304d42d9e96
Rune Skovbo Johansen <rune@unity3d.com>
2016-03-03 16:42:00 +0100

    Todo-note on code that throws error.

--- Input/ActionMap.cs
+++ Input/ActionMap.cs
@@ -65,6 +65,10 @@ namespace UnityEngine.InputNew
 
 		private static void HandlePlayModeCustomizations()
 		{
+			// TODO: Fix error.
+			// get_isPlayingOrWillChangePlaymode is not allowed to be called from a MonoBehaviour constructor,
+			// call it in Awake or Start instead. Called from script 'ActionMap' on game object 'MenuActions'.
+			// See "Script Serialization" page in the Unity Manual for further details.
 			if (EditorApplication.isPlayingOrWillChangePlaymode)
 			{
 				s_IsInPlayMode = true;
