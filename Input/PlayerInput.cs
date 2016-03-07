3afcf8db1ca1b73468599b85280844481fec749d
Rune Skovbo Johansen <rune@unity3d.com>
2016-03-07 15:40:24 +0100

    Cleanup in PlayerInput component.

--- Input/PlayerInput.cs
+++ Input/PlayerInput.cs
@@ -8,44 +8,29 @@ namespace UnityEngine.InputNew
 {
 	public class PlayerInput : MonoBehaviour
 	{
-		public enum DeviceAssignmentStatus
-		{
-			Disabled,
-			Pending,
-			Enabled,
-		}
-
-		public List<ActionMapSlot> actionMaps = new List<ActionMapSlot>();
 		// Should this player handle request assignment of an input device as soon as the component awakes?
 		[FormerlySerializedAs("autoSinglePlayerAssign")]
 		public bool autoAssignGlobal;
 
-		public DeviceAssignmentStatus status { get; private set; }
-		public PlayerHandle handle { get; set; }
+		public List<ActionMapSlot> actionMaps = new List<ActionMapSlot>();
 
+		public PlayerHandle handle { get; set; }
 
 		void Awake()
 		{
 			if (autoAssignGlobal)
-				RequestAssign();
-		}
-
-		void RequestAssign()
-		{
-			if (status == DeviceAssignmentStatus.Enabled)
-				return;
-			
-			handle = InputSystem.GetNewPlayerHandle();
-			handle.global = true;
-			foreach (ActionMapSlot actionMapSlot in actionMaps)
 			{
-				ActionMapInput actionMapInput = ActionMapInput.Create(actionMapSlot.actionMap);
-				actionMapInput.TryInitializeControlSchemeGlobal();
-				actionMapInput.active = actionMapSlot.active;
-				actionMapInput.blockSubsequent = actionMapSlot.blockSubsequent;
-				handle.maps.Add(actionMapInput);
+				handle = InputSystem.GetNewPlayerHandle();
+				handle.global = true;
+				foreach (ActionMapSlot actionMapSlot in actionMaps)
+				{
+					ActionMapInput actionMapInput = ActionMapInput.Create(actionMapSlot.actionMap);
+					actionMapInput.TryInitializeControlSchemeGlobal();
+					actionMapInput.active = actionMapSlot.active;
+					actionMapInput.blockSubsequent = actionMapSlot.blockSubsequent;
+					handle.maps.Add(actionMapInput);
+				}
 			}
-			status = DeviceAssignmentStatus.Enabled;
 		}
 
 		public T GetActions<T>() where T : ActionMapInput
