485ebd2fe5d709a5aa84128f332f0a84465737aa
Rune Skovbo Johansen <rune@unity3d.com>
2016-03-11 11:49:11 +0100

    Reset player handles when exiting play mode.

--- Input/PlayerHandleManager.cs
+++ Input/PlayerHandleManager.cs
@@ -1,15 +1,27 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace UnityEngine.InputNew
 {
 	public static class PlayerHandleManager
 	{
-		static Dictionary<int, PlayerHandle> s_Players = new Dictionary<int, PlayerHandle>();
+		static Dictionary<int, PlayerHandle> s_Players = null;
 		static int s_NextPlayerIndex = 0;
 
 		public static IEnumerable<PlayerHandle> players { get { return s_Players.Values; } }
+
+		static PlayerHandleManager()
+		{
+			Reset();
+
+			#if UNITY_EDITOR
+			EditorApplication.playmodeStateChanged += OnPlaymodeChanged;
+			#endif
+		}
 
 		public static PlayerHandle GetNewPlayerHandle()
 		{
@@ -30,6 +42,28 @@ namespace UnityEngine.InputNew
 		internal static void RemovePlayerHandle(PlayerHandle handle)
 		{
 			s_Players.Remove(handle.index);
+		}
+
+		#if UNITY_EDITOR
+		static void OnPlaymodeChanged()
+		{
+			if (EditorApplication.isPlayingOrWillChangePlaymode)
+				return;
+			Reset();
+		}
+		#endif
+
+		static void Reset()
+		{
+			if (s_Players != null)
+			{
+				List<PlayerHandle> players = new List<PlayerHandle>(s_Players.Values);
+				for (int i = 0; i < players.Count; i++)
+					players[i].Destroy();
+			}
+
+			s_Players = new Dictionary<int, PlayerHandle>();
+			s_NextPlayerIndex = 0;
 		}
 	}
 }
