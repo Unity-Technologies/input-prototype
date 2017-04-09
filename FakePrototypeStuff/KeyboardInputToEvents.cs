d22e49d60be54f49785921cbfa69d14823a24b23
Haruto Watanabe <dailioh@gmail.com>
2017-04-09 14:39:27 +0900

    Avoid catastrophic GC in every frame.

--- FakePrototypeStuff/KeyboardInputToEvents.cs
+++ FakePrototypeStuff/KeyboardInputToEvents.cs
@@ -5,33 +5,34 @@ using UnityEngine.InputNew;
 using Assets.Utilities;
 
 public class KeyboardInputToEvents
-	: MonoBehaviour
+    : MonoBehaviour
 {
-	public void Update()
-	{
-		int controlCount = EnumHelpers.GetValueCount<KeyCode>();
-		for (int i = 0; i < controlCount; i++)
-			HandleKey((KeyCode)i, (KeyCode)i);
-	}
+    private int controlCount = EnumHelpers.GetValueCount<KeyCode>();
 
-	void HandleKey(KeyCode keyCode, KeyCode keyControl)
-	{
-		if (Input.GetKeyDown(keyCode))
-			SendKeyboardEvent(keyControl, true);
-		if (Input.GetKeyUp(keyCode))
-			SendKeyboardEvent(keyControl, false);
-	}
+    public void Update()
+    {
+        for (int i = 0; i < controlCount; i++)
+            HandleKey((KeyCode)i, (KeyCode)i);
+    }
 
-	void SendKeyboardEvent(KeyCode key, bool isDown)
-	{
-		var inputEvent = InputSystem.CreateEvent<KeyboardEvent>();
+    void HandleKey(KeyCode keyCode, KeyCode keyControl)
+    {
+        if (Input.GetKeyDown(keyCode))
+            SendKeyboardEvent(keyControl, true);
+        if (Input.GetKeyUp(keyCode))
+            SendKeyboardEvent(keyControl, false);
+    }
 
-		inputEvent.deviceType = typeof(Keyboard);
-		inputEvent.deviceIndex = 0;
-		inputEvent.key = key;
-		inputEvent.isDown = isDown;
-		////TODO: modifiers
+    void SendKeyboardEvent(KeyCode key, bool isDown)
+    {
+        var inputEvent = InputSystem.CreateEvent<KeyboardEvent>();
 
-		InputSystem.QueueEvent(inputEvent);
-	}
+        inputEvent.deviceType = typeof(Keyboard);
+        inputEvent.deviceIndex = 0;
+        inputEvent.key = key;
+        inputEvent.isDown = isDown;
+        ////TODO: modifiers
+
+        InputSystem.QueueEvent(inputEvent);
+    }
 }
