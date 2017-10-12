ac60ed651e857d41e5b310c7e7f9c44d3575134e
Amy DiGiovanni <amyd@unity3d.com>
2017-10-12 15:46:15 -0700

    Removed commented-out code

--- DeviceProfiles/OpenVRProfile.cs
+++ DeviceProfiles/OpenVRProfile.cs
@@ -1,11 +1,4 @@
-﻿using UnityEngine.InputNew;
-using System.Collections;
-using Assets.Utilities;
-#if ENABLE_STEAMVR_INPUT
-using Valve.VR;
-#endif
-
-namespace UnityEngine.InputNew
+﻿namespace UnityEngine.InputNew
 {
 	public class OpenVRProfile : InputDeviceProfile
 	{
@@ -19,63 +12,6 @@ namespace UnityEngine.InputNew
 
 		public override void Remap(InputEvent inputEvent)
 		{
-
-//			var controlEvent = inputEvent as GenericControlEvent;
-
-//            if (controlEvent != null)
-//			{
-//                // Debug.Log("Remapping: " + inputEvent + " Control Index: " + controlEvent.controlIndex);
-
-//                switch (controlEvent.controlIndex) {
-//                    // Axes
-//                    case 0: // Axis 0.X / trackpad X
-//                        controlEvent.controlIndex = (int) XRInputDevice.TrackedInputDeviceControl.LeftStickX;
-//                        break;
-//                    case 1: // Axis 0.Y / trackpad Y
-//                        controlEvent.controlIndex = (int)XRInputDevice.TrackedInputDeviceControl.LeftStickY;
-//                        break;
-//                    case 2: // Trigger (Axis 1.X)
-//                        controlEvent.controlIndex = (int)XRInputDevice.TrackedInputDeviceControl.Trigger1;
-//                        break;
-//                    case 3:
-//                        controlEvent.controlIndex = (int)XRInputDevice.TrackedInputDeviceControl.Analog4;
-//                        break;
-//                    case 4:
-//                        controlEvent.controlIndex = (int)XRInputDevice.TrackedInputDeviceControl.Analog5;
-//                        break;
-//                    case 5:
-//                        controlEvent.controlIndex = (int)XRInputDevice.TrackedInputDeviceControl.Analog6;
-//                        break;
-//                    case 6:
-//                        controlEvent.controlIndex = (int)XRInputDevice.TrackedInputDeviceControl.Analog7;
-//                        break;
-//                    case 7:
-//                        controlEvent.controlIndex = (int)XRInputDevice.TrackedInputDeviceControl.Analog8;
-//                        break;
-//                    case 8:
-//                        controlEvent.controlIndex = (int)XRInputDevice.TrackedInputDeviceControl.Analog9;
-//                        break;
-//                    case 9:
-//                        controlEvent.controlIndex = (int)XRInputDevice.TrackedInputDeviceControl.Analog9;
-//                        break;
-//#if ENABLE_STEAMVR_INPUT
-//					// Buttons
-//					case kViveAxisCount + (int)EVRButtonId.k_EButton_SteamVR_Trigger:
-//                        controlEvent.controlIndex = (int)XRInputDevice.TrackedInputDeviceControl.Trigger1;
-//                        break;
-//	                case kViveAxisCount + (int)EVRButtonId.k_EButton_Axis2: // Grip on Oculus Touch
-//					case kViveAxisCount + (int)EVRButtonId.k_EButton_Grip:
-//                        controlEvent.controlIndex = (int)XRInputDevice.TrackedInputDeviceControl.Trigger2;
-//                        break;
-//                    case kViveAxisCount + (int)EVRButtonId.k_EButton_SteamVR_Touchpad:
-//                        controlEvent.controlIndex = (int) XRInputDevice.TrackedInputDeviceControl.LeftStickButton;
-//                        break;
-//                    case kViveAxisCount + (int)EVRButtonId.k_EButton_ApplicationMenu:
-//                        controlEvent.controlIndex = (int)XRInputDevice.TrackedInputDeviceControl.Action2;
-//                        break;
-//#endif
-//                }
-//            }
 		}
 	}
 
