44b10a5b8db84b09729fc98a8767232d1636a7e0
Matt Schoen <mtschoen@gmail.com>
2019-06-06 17:46:27 -0700

    Rename the EditorXR assembly from EXR to Unity.Labs.EditorXR

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,4 +3,4 @@
 // This is necessary to allow cross-communication between assemblies for classes in the UnityEditor.Experimental.EditorVR
 // namespace; The plan is that all code will eventually be within a single assembly.
 
-[assembly: InternalsVisibleTo("EXR")]
+[assembly: InternalsVisibleTo("Unity.Labs.EditorXR")]
